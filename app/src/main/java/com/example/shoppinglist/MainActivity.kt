1f09617050c1c777414f90fdf9a92a1b9750151c
Jamie10X <jamshidboynazarov0@gmail.com>
2023-12-30 22:14:00 +0900

    ADD item dialog screen and other classes

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,22 +1,50 @@
 package com.example.shoppinglist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.data.database.ShoppingDatabase
+import com.example.shoppinglist.data.database.entities.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
+import com.example.shoppinglist.other.ShoppingItemAdapter
+import com.example.shoppinglist.ui.shoppingList.AddDialogListener
+import com.example.shoppinglist.ui.shoppingList.AddShoppingItemDialog
 import com.example.shoppinglist.ui.shoppingList.ShoppingViewModel
 import com.example.shoppinglist.ui.shoppingList.ShoppingViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityShoppingBinding
+
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val database = ShoppingDatabase(this)
         val repository = ShoppingRepository(database)
         val factory = ShoppingViewModelFactory(repository)
 
-        val viewModel = ViewModelProvider(this, factory).get(ShoppingViewModel::class.java)
+        val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+        binding.shoppingitems.layoutManager = LinearLayoutManager(this)
+        binding.shoppingitems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this) {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        }
+        binding.flobtn.setOnClickListener{
+            AddShoppingItemDialog(this,
+        object : AddDialogListener {
+            override fun onAddButtonClicked(item: ShoppingItem) {
+            viewModel.upsert(item)
+            }
+        })
+                .show()
+        }}
     }
-}
--- app/src/main/java/com/example/shoppinglist/ui/shoppingList/AddShoppingItemDialog.kt
+++ app/src/main/java/com/example/shoppinglist/ui/shoppingList/AddShoppingItemDialog.kt
@@ -5,25 +5,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
+import com.example.shoppinglist.R
 import com.example.shoppinglist.data.database.entities.ShoppingItem
 import com.example.shoppinglist.databinding.DialogAdditemBinding
 
 
 
-class AddShoppingItemDialog (context: Context) : AppCompatDialog(context){
+class AddShoppingItemDialog(context: Context, private val addDialogListener: AddDialogListener) : AppCompatDialog(context) {
     private lateinit var binding: DialogAdditemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DialogAdditemBinding.inflate(LayoutInflater)
+        binding = DialogAdditemBinding.inflate(LayoutInflater.from(context))
         setContentView(binding.root)
-        binding.tvAdd.setOnClickListener{
+        setContentView(R.layout.shopping_items)
+
+        binding.tvAdd.setOnClickListener {
             val name = binding.etName.text.toString()
             val amount = binding.etAmount.text.toString()
-            if (name.isEmpty() || amount.isEmpty()){
+            if (name.isEmpty() || amount.isEmpty()) {
                 Toast.makeText(context, "Please enter all information", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            val item = ShoppingItem
+
+            // Additional validation can be added here, e.g., checking if amount is a valid number
+
+            val item = ShoppingItem(name, amount.toInt())
+            addDialogListener.onAddButtonClicked(item)
+            dismiss()
+        }
+
+        binding.tvCancel.setOnClickListener {
+            cancel()
         }
     }
 }
